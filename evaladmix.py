73a36c36fe64e6f5acbde1f5091577bc24337aba
stevemussmann <smussmann@gmail.com>
2021-04-10 11:28:07 -0600

    began EvalAdmix class

---
+++ evaladmix.py
@@ -0,0 +1,60 @@
+from evalAdmixComline import ComLine
+from syscall import SysCall
+
+from rpy2.robjects import StrVector
+from rpy2.robjects.packages import STAP
+from rpy2.robjects.packages import importr
+from rpy2.robjects import r
+
+import sys
+
+class EvalAdmix():
+	'Class for executing evalAdmix commands'
+
+	def __init__(self,prefix):
+		self.prefix = prefix
+
+	def evalAdmix(self, k, np):
+		qf = self.prefix + "." + str(k) + "_1.Q"
+		eAf = self.prefix + "." + str(k) + "_1.corres"
+		evalAdmix_str_com = "evalAdmix -plink " + self.prefix + " -fname " + self.prefix + "." + str(k) + "_1.P -qname " + qf + " -o " + eAf + " -P " + str(np)
+
+		call = SysCall(evalAdmix_str_com)
+		call.run_program()
+
+	def Rcode(self, funcs, k):
+	
+		# import R functions
+		utils = importr('utils')
+		base = importr('base')
+		grdevices = importr('grDevices')
+
+		# import R plotting functions from evalAdmix
+		with open(funcs, 'r') as f:
+			string = f.read()
+		myfunc = STAP(string, "myfunc")
+
+		#make file names
+		famf = self.prefix + ".fam"
+		qscoresf = self.prefix + "." + str(k) + "_1.Q"
+		eAf = self.prefix + "." + str(k) + "_1.corres"
+		output = eAf + ".png"
+
+		# read in files
+		pop = base.as_matrix(utils.read_table(famf))
+		q = utils.read_table(qscoresf)
+		cor = base.as_matrix(utils.read_table(eAf))
+
+		print(pop)
+		print(type(pop))
+		print(pop.rx(True,2))
+		#print(q)
+
+		# run plotting functions
+		ordr = myfunc.orderInds(pop=base.as_vector(pop.rx(True,2)), q=q)
+		print(type(ordr))
+		print(ordr)
+
+		grdevices.png(file=output)
+		myfunc.plotCorRes(cor_mat=cor, pop=base.as_vector(pop.rx(True,2)), ord=ordr, title="test", max_z=0.1, min_z=-0.1)
+		grdevices.dev_off()
--- runEvalAdmix.py
+++ runEvalAdmix.py
@@ -1,61 +1,10 @@
 #!/usr/bin/env python3
 
 from evalAdmixComline import ComLine
-from syscall import SysCall
+from evaladmix import EvalAdmix
 from plink import Plink
 
-from rpy2.robjects import StrVector
-from rpy2.robjects.packages import STAP
-from rpy2.robjects.packages import importr
-from rpy2.robjects import r
-
 import sys
-
-def evalAdmix(fprefix, k, np):
-	qf = fprefix + "." + str(k) + "_1.Q"
-	eAf = fprefix + "." + str(k) + "_1.corres"
-	evalAdmix_str_com = "evalAdmix -plink " + fprefix + " -fname " + fprefix + "." + str(k) + "_1.P -qname " + qf + " -o " + eAf + " -P " + str(np)
-
-	call = SysCall(evalAdmix_str_com)
-	call.run_program()
-
-def Rcode(funcs, fprefix, k):
-	
-	# import R functions
-	utils = importr('utils')
-	base = importr('base')
-	grdevices = importr('grDevices')
-
-	# import R plotting functions from evalAdmix
-	with open(funcs, 'r') as f:
-		string = f.read()
-	myfunc = STAP(string, "myfunc")
-
-	#make file names
-	famf = fprefix + ".fam"
-	qscoresf = fprefix + "." + str(k) + "_1.Q"
-	eAf = fprefix + "." + str(k) + "_1.corres"
-	output = eAf + ".png"
-
-	# read in files
-	pop = base.as_matrix(utils.read_table(famf))
-	q = utils.read_table(qscoresf)
-	cor = base.as_matrix(utils.read_table(eAf))
-
-	print(pop)
-	print(type(pop))
-	print(pop.rx(True,2))
-	#print(q)
-
-	# run plotting functions
-	ordr = myfunc.orderInds(pop=base.as_vector(pop.rx(True,2)), q=q)
-	print(type(ordr))
-	print(ordr)
-
-	grdevices.png(file=output)
-	myfunc.plotCorRes(cor_mat=cor, pop=base.as_vector(pop.rx(True,2)), ord=ordr, title="test", max_z=0.1, min_z=-0.1)
-	grdevices.dev_off()
-
 
 def main():
 	input = ComLine(sys.argv[1:])
@@ -65,11 +14,9 @@ def main():
 	pl.makeBED()
 	pl.fixFam(input.args.popmap)
 
-	evalAdmix(input.args.prefix, input.args.testK, input.args.np)
-	Rcode(input.args.evalAdmixRcode, input.args.prefix, input.args.testK)
-
-
-	
+	ea = EvalAdmix(input.args.prefix)
+	ea.evalAdmix(input.args.testK, input.args.np)
+	ea.Rcode(input.args.evalAdmixRcode, input.args.testK)
 
 main()
 

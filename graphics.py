ab1824f160320817811c428818732cd2e9a49d7c
stevemussmann <smussmann@gmail.com>
2021-04-24 11:04:21 -0600

    cvSum.py plot now properly sorted; axis labels rotated 90 degrees

--- graphics.py
+++ graphics.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot
 import os
 import pandas
+import re
+import natsort as ns
+
+def natural_sort(l): 
+	convert = lambda text: int(text) if text.isdigit() else text.lower() 
+	alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+	return sorted(l, key = alphanum_key)
 
 class Graphics():
 	'Class to print plots of data'
@@ -13,7 +20,15 @@ class Graphics():
 	def printFigure(self):
 		#make pandas dataframe to include NaN values
 		df = pandas.DataFrame(dict([ (k,pandas.Series(v)) for k,v in self.data.items() ]), dtype=float)
+		df.sort_index(axis=1,inplace=True)
+		c = df.columns
+		#c = sorted(ns.natsorted(c), key=str.isdigit, reverse=True)
+		c = natural_sort(c)
+		df = df[c]
 		print(df)
 		fig = matplotlib.pyplot.figure(figsize=(12.8,9.6),dpi=300,frameon=False)
 		df.boxplot()
+		matplotlib.pyplot.grid(b='None')
+		matplotlib.pyplot.xticks(rotation=90)
 		fig.savefig(self.output, bbox_inches='tight')
+

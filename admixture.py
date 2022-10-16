c9743249c5ba160d4ebd63c7eb83f08be3d73627
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2022-10-15 20:15:53 -0700

    Improved handling of Admixture output locations

--- admixture.py
+++ admixture.py
@@ -8,8 +8,10 @@ import csv
 import json
 import os
 import os.path
+import shutil
 import subprocess
 import sys
+import time
 import numpy as np
 import zipfile
 
@@ -28,6 +30,10 @@ class Admixture():
 		self.ext = ".ped"
 		if bed == True:
 			self.ext = ".bed"
+		self.infile = self.prefix + self.ext #build input file name based on ped/bed prefix and proper extension
+		self.startdir = os.getcwd() #directory from which admixturePipeline.py was launched
+		self.abspath = os.path.abspath(self.infile) #absolute path to infile
+		self.outdir = os.path.dirname(self.abspath) #absolute path to directory containing infile
 
 	def admix(self):
 		ks = range(self.minK, self.maxK+1)
@@ -35,21 +41,48 @@ class Admixture():
 		#for each k value
 		for i in ks:
 			for j in range(self.rep):
-				command_string = "admixture -j" + str(self.NP) + " -s " + str(np.random.randint(1000000)) + " --cv=" + str(self.cv) + " " + self.prefix + self.ext + " " + str(i)
+				command_string = "admixture -j" + str(self.NP) + " -s " + str(np.random.randint(1000000)) + " --cv=" + str(self.cv) + " " + str(self.abspath) + " " + str(i)
 				
+				#make a temporary directory with name based on current time in milliseconds
+				curtime = str(round(time.time() * 1000)) #get current time for naming directory
+				newdir = os.path.join(self.outdir, curtime) #construct path to temporary directory
+				os.mkdir(newdir) #make directory
+
+				#move into temporary directory
+				os.chdir(newdir)
+
+
 				#call Admixture
-				admixtureCall = SysCall(command_string)
-				admixtureCall.run_admixture(self.prefix,i,j)
+				admixtureCall = SysCall(command_string) #prepare system call
+				pathlist = os.path.split(self.prefix)
+				strippedPrefix = pathlist[1] #get input file prefix - this code should still work if a relative path was used, or if the input file is in the same directory from which admixturePipeline.py was launched
+				newprefix = os.path.join(self.outdir, strippedPrefix) #this 'prefix' includes the absolute path to the output directory
+				admixtureCall.run_admixture(newprefix,i,j) #execute Admixture for k=i, rep=j
 
 				#Manually re-name output files to include _j rep number
-				for filename in os.listdir("."):
-					fn = self.prefix + "." + str(i) + "."
+				counter = 0 #counter to track whether output files were found. Correct final number = 2
+				for filename in os.listdir(newdir):
+					fn = strippedPrefix + "." + str(i) + "."
 					if fn in filename:
+						counter+=1
 						oldname, extension = os.path.splitext(filename)
 						newname = oldname + "_" + str(j) + extension
+						newpath = os.path.join(self.outdir, newname)
+						#print(newname)
 						if(extension.endswith("Q")):
 							self.qfiles[str(i)].append(newname)
-						os.rename(filename, newname)
+						os.rename(filename, newpath)
+				if counter != 2:
+					print("ERROR: Appropriate number of Admixture output files (N=2) was not found")
+					print("This error originated from somewhere around line 64 in admixture.py")
+					print("")
+					raise SystemExit
+
+				#move back to original directory
+				os.chdir(self.startdir)
+
+				#remove temporary directory
+				shutil.rmtree(newdir) #need to use shutil.rmtree() because os.rmdir only deletes empty directories 
 
 		# write dict of .Q files
 		jsonFile=self.prefix + ".qfiles.json"
@@ -57,51 +90,15 @@ class Admixture():
 			json.dump(self.qfiles, json_file)
 
 	def zipdir(self,path,ziph):
-		files = [f for f in os.listdir('.') if os.path.isfile(f)]
+		files = [f for f in os.listdir(self.outdir) if os.path.isfile(f)]
 		for root,dirs,files in os.walk(path, topdown=True):
 			[dirs.remove(d) for d in list(dirs)]
 			for f in files:
 				if f.endswith('.Q'):
-					ziph.write(os.path.join(root,f))
+					ziph.write(os.path.join(root,f), f)
 
 	def create_zip(self):
-		zipf = zipfile.ZipFile('results.zip', 'w', zipfile.ZIP_DEFLATED)
-		self.zipdir('./', zipf)
+		resultspath = os.path.join(self.outdir, "results.zip")
+		zipf = zipfile.ZipFile(resultspath, 'w', zipfile.ZIP_DEFLATED)
+		self.zipdir(self.outdir, zipf)
 		zipf.close()
-
-#	def print_cv(self):
-#		print("Printing CV values...")
-#		command="grep -h CV " + self.prefix + "*.stdout > " + self.prefix + "_cv_summary.txt"
-#
-#		grepCall = SysCall(command)
-#		grepCall.run_program()
-#
-#	def loglik(self):
-#		fh = open("loglik.txt", 'wb')
-#		for fn in os.listdir("."):
-#			if fn.endswith("stdout"):
-#				temp = open(fn, 'r')
-#				fnlist = fn.split("_")
-#				fnlist2 = fnlist[-2].split(".")
-#				kval = fnlist2[-1]
-#				runNums = fnlist[-1].split(".")
-#				runNum = runNums[0]
-#				print(fnlist2)
-#				for line in temp.readlines():
-#					if line.startswith("Loglikelihood:"):
-#						mylist = line.split()
-#						#print(mylist)
-#						fh.write(kval.encode())
-#						fh.write("\t".encode())
-#						fh.write(runNum.encode())
-#						fh.write("\t".encode())
-#						fh.write(mylist[-1].encode())
-#						fh.write("\n".encode())
-#				temp.close()
-#		fh.close()
-#
-#		print("Sorting log(likelihood) values...")
-#		command="sort -n -k1 -o loglik.txt loglik.txt"
-#
-#		sortCall = SysCall(command)
-#		sortCall.run_program()

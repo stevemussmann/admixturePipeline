a345179b1853bef4f5338771112cc061fc81caba
Steve Mussmann <17727337+stevemussmann@users.noreply.github.com>
2022-11-24 16:29:58 -0600

    Docker build with local CLUMPAK install

--- Docker/buildDocker.sh
+++ Docker/buildDocker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker image build --no-cache -t admixpipe --file Dockerfile .
+docker image build --platform linux/amd64 --no-cache -t admixpipe --file Dockerfile .
+#docker buildx build --platform linux/amd64 --no-cache -t admixpipe --file Dockerfile .
 #docker image build -t admixpipe --file Dockerfile .
 
 
 exit
+
